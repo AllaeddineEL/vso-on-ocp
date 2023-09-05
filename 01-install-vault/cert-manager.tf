ad324cfc73b88ec18e8b36e2d05dd990883d15a1
AllaeddineEL <a.elareed@gmail.com>
2023-09-05 13:48:28 +0200

    refactor cluster issuer resource

--- 01-install-vault/cert-manager.tf
+++ 01-install-vault/cert-manager.tf
@@ -34,17 +34,45 @@ resource "time_sleep" "wait" {
   depends_on = [helm_release.cert_manager]
 }
 
-resource "kubectl_manifest" "cluster_issuer" {
-  validate_schema = false
-  yaml_body       = templatefile("${path.module}/templates/cert-manager/clusterissuer.yaml", { proj_id = var.project_id, domain_name = "${trimsuffix(data.google_dns_managed_zone.doormat_dns_zone.dns_name, ".")}" })
-  depends_on      = [time_sleep.wait]
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name"      = "letsencrypt-prod"
+      "namespace" = "cert-manager"
+    }
+    "spec" = {
+      "acme" = {
+        "email" = "nobody@hashicorp.com"
+        "privateKeySecretRef" = {
+          "name" = "letsencrypt-prod"
+        }
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "http01" = null
+            "ingress" = {
+              "class" = "openshift-default"
+            }
+          },
+        ]
+      }
+    }
+  }
 }
 
-resource "kubectl_manifest" "wildcard_certifcate" {
-  validate_schema = false
-  yaml_body       = templatefile("${path.module}/templates/cert-manager/certificate.yaml", { domain_name = "${trimsuffix(data.google_dns_managed_zone.doormat_dns_zone.dns_name, ".")}" })
-  depends_on      = [kubectl_manifest.cluster_issuer]
-}
+# resource "kubectl_manifest" "cluster_issuer" {
+#   validate_schema = false
+#   yaml_body       = templatefile("${path.module}/templates/cert-manager/clusterissuer.yaml", { proj_id = var.project_id, domain_name = "${trimsuffix(data.google_dns_managed_zone.doormat_dns_zone.dns_name, ".")}" })
+#   depends_on      = [time_sleep.wait]
+# }
+
+# resource "kubectl_manifest" "wildcard_certifcate" {
+#   validate_schema = false
+#   yaml_body       = templatefile("${path.module}/templates/cert-manager/certificate.yaml", { domain_name = "${trimsuffix(data.google_dns_managed_zone.doormat_dns_zone.dns_name, ".")}" })
+#   depends_on      = [kubectl_manifest.cluster_issuer]
+# }
 # resource "kubernetes_manifest" "cluster_issuer" {
 #   manifest   = yamldecode(templatefile("${path.module}/templates/clusterissuer.yaml", { proj_id = var.project_id, domain_name = "*.${trimsuffix(data.google_dns_managed_zone.doormat_dns_zone.dns_name, ".")}" }))
 #   depends_on = [helm_release.cert_manager, time_sleep.wait]
