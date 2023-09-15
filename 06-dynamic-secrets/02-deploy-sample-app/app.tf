089edc471abe008e8383ddeac6581231affa9af1
AllaeddineEL <a.elareed@gmail.com>
2023-09-15 08:43:45 +0200

    add dynamic secrets config

---
+++ 06-dynamic-secrets/02-deploy-sample-app/app.tf
@@ -0,0 +1,158 @@
+
+resource "kubectl_manifest" "vault-auth-default" {
+  validate_schema = false
+  yaml_body       = <<YAML
+  apiVersion: secrets.hashicorp.com/v1beta1
+  kind: VaultAuth
+  metadata:
+    name: default
+    namespace: dev-backend
+  spec:
+    method: kubernetes
+    mount: "dev-backend-auth-mount"
+    namespace: "dev/backend"
+    kubernetes:
+      role: "dev-backend-postgres"
+      serviceAccount: default
+      audiences:
+        - vault
+  YAML
+}
+
+resource "kubectl_manifest" "vault-dynamic-secret" {
+  validate_schema = false
+  yaml_body       = <<YAML
+  apiVersion: secrets.hashicorp.com/v1beta1
+  kind: VaultDynamicSecret
+  metadata:
+    name: vso-db-demo
+    namespace: dev-backend
+  spec:
+    vaultAuthRef: ${kubectl_manifest.vault-auth-kv.name}
+    namespace: "dev/backend"
+    mount: "dev-backend-db"
+    path: "creds/dev-backend-postgres"
+    destination:
+      create: false
+      name: ${kubernetes_secret.db.metadata[0].name}
+    rolloutRestartTargets:
+      - kind: Deployment
+        name: vso-db-demo 
+  YAML
+}
+
+resource "kubectl_manifest" "vault-dynamic-secret-create" {
+  validate_schema = false
+  yaml_body       = <<YAML
+  apiVersion: secrets.hashicorp.com/v1beta1
+  kind: VaultDynamicSecret
+  metadata:
+    name: vso-db-demo-create
+    namespace: dev-backend
+  spec:
+    vaultAuthRef: ${kubectl_manifest.vault-auth-kv.name}
+    namespace: "dev/backend"
+    mount: "dev-backend-db"
+    path: "creds/dev-backend-postgres"
+    destination:
+      create: true
+      name: vso-db-demo-created
+    rolloutRestartTargets:
+      - kind: Deployment
+        name: vso-db-demo 
+  YAML
+}
+
+resource "kubernetes_secret" "db" {
+  metadata {
+    name      = "vso-db-demo"
+    namespace = "dev-backend"
+  }
+}
+
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name      = "vso-db-demo"
+    namespace = "dev-backend"
+    labels = {
+      test = "vso-db-demo"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    strategy {
+      rolling_update {
+        max_unavailable = "1"
+      }
+    }
+
+    selector {
+      match_labels = {
+        test = "vso-db-demo"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "vso-db-demo"
+        }
+      }
+
+      spec {
+        volume {
+          name = "secrets"
+          secret {
+            secret_name = kubernetes_secret.db.metadata[0].name
+          }
+        }
+        container {
+          image = "postgres:latest"
+          name  = "demo"
+          command = [
+            "sh", "-c", "while : ; do psql postgresql://$PGUSERNAME@${local.postgres_host}/postgres?sslmode=disable -c 'select 1;' ; echo $PGPASSWORD ;sleep 5; done"
+          ]
+
+          env {
+            name = "PGPASSWORD"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.db.metadata[0].name
+                key  = "password"
+              }
+            }
+          }
+
+          env {
+            name = "PGUSERNAME"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.db.metadata[0].name
+                key  = "username"
+              }
+            }
+          }
+
+          volume_mount {
+            name       = "secrets"
+            mount_path = "/etc/secrets"
+            read_only  = true
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "64Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ 06-dynamic-secrets/02-deploy-sample-app/providers.tf
@@ -0,0 +1,18 @@
+
+provider "vault" {
+
+}
+provider "kubernetes" {
+  experiments {
+    manifest_resource = true
+  }
+  config_path = "~/.kube/config"
+}
+provider "kubectl" {
+  config_path = "~/.kube/config"
+}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
